6045dc393739523ae5302704e8a71570e2d5607b
Henrique Barcellos Lima <85499980+HenriqueeLimaa@users.noreply.github.com>
2022-06-06 18:43:35 -0300

    Change class name to App and delete jUnit stuff

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    
+
+            //Adapter Adapter = new Adapter();
+            //Adapter.add(Position.First, "A");
+            //Adapter.add(Position.Last, "b");
+            //Adapter.add(Position.First, "C");
+        
+        
+        
+        
+}
