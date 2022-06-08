4b07f0b67113b59fa3d4c4289d481d16b5e832c1
Letícia Bento do Carmo <leticiac@dbserver.com.br>
2022-06-08 14:42:37 -0300

    remove item e printa o resultado

--- App.java
+++ App.java
@@ -1,16 +1,20 @@
 public class App {
-    
+
     public static void main(String[] args) {
-    
+
         Adapter Adapter = new Adapter();
-    
+
         Adapter.add(Position.First, "A");
         Adapter.add(Position.Last, "b");
         Adapter.add(Position.First, "C");
-        
+
         System.out.println("getFirst:");
-        System.out.printf("esperado: C; obtido %s",Adapter.get(Position.First));
+        System.out.printf("esperado: C; obtido %s", Adapter.get(Position.First));
+
+        System.out.println("\nremove: C");
+        Adapter.remove(Position.First, "C\n");
+        System.out.println("getFirst:");
+        System.out.printf("esperado: A; \nobtido %s\n", Adapter.get(Position.First));
     }
-        
-        
+
 }
